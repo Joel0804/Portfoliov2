7af0f4653b55cd9edfabcd56cdaff746cd81b284
Joel Dsilva <joeldsilva0804@gmail.com>
2026-06-17 08:21:14 +0530

    contains main file

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfoliov2-ayan.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
